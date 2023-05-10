a6c5d2616f88e5cd7cc84527027276701ddd7716
Shipann <artem.shibcko2012@gmail.com>
2023-05-10 16:14:06 +0300

    hm4

--- HomeWork3/Program.cs
+++ HomeWork3/Program.cs
@@ -4,7 +4,8 @@
     {
         static void Main(string[] args)
         {
-            string str = "loremipsum lalwdj9aoikwdjlaskdhjwajksdniala";
+           //Task 1
+            /*string str = "loremipsum lalwdj9aoikwdjlaskdhjwajksdniala";
 
             int a = 0;
             int o = 0;
@@ -27,6 +28,64 @@
             Console.WriteLine("O = " + o);
             Console.WriteLine("I = " + i);
             Console.WriteLine("E = " + e);
+
+            //Task 2
+            Console.WriteLine("Enter the number of the month 1-12: ");
+            int monthNumber = int.Parse(Console.ReadLine());
+
+            int daysInMonth;
+
+            switch (monthNumber)
+            {
+                case 2:
+                    daysInMonth = 28;
+                    break;
+                case 4:
+                case 6:
+                case 9:
+                case 11:
+                    daysInMonth = 30;
+                    break;
+                default:
+                    daysInMonth = 31;
+                    break;
+            }
+
+            Console.WriteLine("Number of days in the month: " + daysInMonth);
+            */
+            // Task 3
+            int[] numbers = new int[10];
+
+            Console.WriteLine("Enter 10 numbers:");
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] = int.Parse(Console.ReadLine());
+            }
+
+            int sum = 0;
+            int product = 1;
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (i < 5 && numbers[i] > 0)
+                {
+                    sum += numbers[i];
+                }
+                else if (i >= 5)
+                {
+                    product *= numbers[i];
+                }
+            }
+
+            if (sum > 0)
+            {
+                Console.WriteLine("Sum  " + sum);
+            }
+            else
+            {
+                Console.WriteLine("Product " + product);
+            }
         }
     }
 }
